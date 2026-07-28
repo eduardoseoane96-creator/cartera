ca3c678e17f78f626baa29b325092920e5787ff2
eduardoseoane96-creator <eduardoseoane96@gmail.com>
2026-07-28 02:46:31 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service worker: cachea la app para que abra al instante y funcione sin señal.
    Los datos de la planilla NO se cachean acá (van a localStorage). */
 
-const CACHE = 'cartera-v3';
+const CACHE = 'cartera-v4';
 const SHELL = [
   './',
   './index.html',
